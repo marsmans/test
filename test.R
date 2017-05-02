44f2a3816eea09a6add53e912bed71349a5e9e65
marsmans <s.marsman91@gmail.com>
2017-05-02 09:40:45 +0200

    plaatje gemaakt

--- test.R
+++ test.R
@@ -1,2 +1,18 @@
 # dit is een test voor github
-setwd("~/disks/y/ontwapps/Timer/Users/Stijn/Model/test")
+setwd("~/disks/y/ontwapps/Timer/Users/Stijn/Model/")
+
+# data inlezen
+cumuvstempLL <- read.csv(file = "Databases/cumuvstemp_lowerlimit.txt", header = TRUE)
+cumuvstempUL <- read.csv(file = "Databases/cumuvstemp_upperlimit.txt", header = TRUE)
+
+# plaatje
+plot(cumuvstempLL,xlim=c(0,9000))
+# rechte lijn best fit
+fLL <- lm(data = cumuvstempLL, temp ~ cumuCO2)
+abline(fLL)
+
+# plaatje
+points(cumuvstempUL,xlim=c(0,9000))
+# rechte lijn best fit
+fUL <- lm(data = cumuvstempUL, temp ~ cumuCO2)
+abline(fUL)
